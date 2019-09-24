9efc24718ca1ff8cd074ef28240a195f75e7dd50
Alexander Dada <alexdada555@gmail.com>
2019-09-24 01:40:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const exampleEmbed = new Discord.RichEmbed();
-
 const prefix = "-";
 
 client.once('ready', () =>
@@ -69,12 +67,6 @@ client.on('message', message =>                                                 
 		const intro  = message.guild.channels.find(ch => ch.name === 'introduction'); // find the intro, roles and welcome chanlles
 		const roles = message.guild.channels.find(ch => ch.name === 'roles');
 
-		exampleEmbed.setColor('#0099ff');
-		exampleEmbed.setTitle('Some title');
-		exampleEmbed.setURL('https://discord.js.org/');
-		exampleEmbed.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org');
-		exampleEmbed.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
 		if(!welcome || !intro) return;                                               // do nothing if they arent 
 		if(!roles) 
 		{	
@@ -83,14 +75,53 @@ client.on('message', message =>                                                 
 		else
 		{
 			//sysChannel.send(` \n Welcome to ${serverName}, ${user.displayName} ! \n Please read:  ${welcome} \n leave an intro in: ${intro} \n Get your Role from: ${roles} \n  `);                 // return welcome messgae to member log channel
-			message.channel.send({embed: {
-				color: 3447003,
-				description: "A very simple Embed!"
-			  }});
-			sysChannel.send({embed: {
-				color: 3447003,
-				description: "A very simple Embed!"
-			}});
+			const exampleEmbed = {
+				color: 0x0099ff,
+				title: ` Welcome ${user.displayName}, To The ${serverName} Discord Server`,
+				url: 'https://primotechorg.wordpress.com/',
+				author: {
+					name: 'Some name',
+					icon_url: 'https://i.imgur.com/wSTFkRM.png',
+					url: 'https://discord.js.org',
+				},
+				description: 'Some description here',
+				thumbnail: {
+					url: 'https://i.imgur.com/wSTFkRM.png',
+				},
+				fields: [
+					{
+						name: 'Welcome Channel',
+						value: `${welcome}`,
+					},
+					{
+						name: 'Introduction Channel',
+						value: `${intro}`,
+					},
+					{
+						name: 'Roles Channel',
+						value: `${roles}`,
+					},
+					{
+						name: '\u200b',
+						value: '\u200b',
+					},
+					{
+						name: 'Inline field title',
+						value: 'Some value here',
+						inline: true,
+					},
+				],
+				image: {
+					url: 'https://i.imgur.com/wSTFkRM.png',
+				},
+				timestamp: new Date(),
+				footer: {
+					text: 'Some footer text here',
+					icon_url: 'https://i.imgur.com/wSTFkRM.png',
+				},
+			};
+			
+			sysChannel.send({ embed: exampleEmbed });
 		}
 	}
 	else if(message.content.startsWith(`${prefix}addrole`))
