8725f2b0c563c514dbd301225c8d3b6eaebd6816
Alexander Dada <alexdada555@gmail.com>
2019-09-28 05:38:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ client.on('message', message =>                                                 
 		var rolesRaw = message.guild.roles.array();
 		var i,j = 0;
 		var Roles = rolesRaw.filter(r => r != '@everyone');
-		message.channel.send("```\n" +Roles+ "\n```");
+		message.channel.send("```\n" +Roles.name+ "\n```");
 
 		/*for(i=0;i<rolesRaw.length;i++)
 		{
